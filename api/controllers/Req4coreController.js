abecb5f796d74909de64e6bf2942878c2abb325e
my506 <my506@163.com>
2017-01-24 17:15:11 +0800

    增加核心请求及返回报文

---
+++ api/controllers/Req4coreController.js
@@ -0,0 +1,11 @@
+/**
+ * Req4coreController
+ *
+ * @description :: Server-side logic for managing req4cores
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/Res4coreController.js
@@ -0,0 +1,11 @@
+/**
+ * Res4coreController
+ *
+ * @description :: Server-side logic for managing res4cores
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
